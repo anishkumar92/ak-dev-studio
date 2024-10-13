82f3c9bc1274a0c173b5646cdb3901eff5f30490
Anish <anishkumar92@gmail.com>
2024-10-13 13:49:33 +0530

    intial comit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+
+const routes: Routes = [
+  { path: '', component: ComingSoonComponent },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
