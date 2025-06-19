2f6cba739e2a0cb16501b18fa8b0bf6b804df08a
Anish Kumar Srinivaasan <anishkumar92@gmail.com>
2025-06-19 15:07:53 +0530

    setting up landing pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: '', component: ComingSoonComponent },
+  { path: '', component: LandingPageComponent },
   { path: '**', redirectTo: '' }
 ];
 
